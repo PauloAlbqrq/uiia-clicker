c30e3492125a51744ef988d82047fd57d1f3439c
joaomonteandrade <joaomontea@gmail.com>
2025-08-26 21:00:45 -0400

    só pra salvar mesmo

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext("2d")
 
+import {Vector2} from './js/Vector2.js'
+
 function resizeCanvas() {
       const windowWidth = window.innerWidth;
       const windowHeight = window.innerHeight;
@@ -15,16 +17,13 @@ function resizeCanvas() {
 window.addEventListener("resize", resizeCanvas);
 resizeCanvas(); 
 
-var x = 10
-
 const img = new Image()
-img.src = "https://mario.wiki.gallery/images/9/97/YoshisIsland2.png"
+img.src = "https://www.spriters-resource.com/media/assets/221/224030.png?updated=1755490532"
 
 function draw() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height)
 
 	ctx.drawImage(img, 0, 0)
-	x++
 
 	requestAnimationFrame(draw)
 }
